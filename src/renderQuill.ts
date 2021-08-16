0f6fb86d5f5f356e81e7965e44825e71b195c78e
Nathan Faucett <nathanfaucett@gmail.com>
2021-08-16 17:25:25 -0400

    fix renderer highlight

--- src/renderQuill.ts
+++ src/renderQuill.ts
@@ -4,9 +4,18 @@ import type Op from "quill-delta/dist/Op";
 import { createQuill } from "./createQuill";
 
 export function renderQuill(node: HTMLElement, quill: Quill) {
-  node.innerHTML = `<div class="ql-container ql-snow ql-rich-editor"><div class="ql-editor">${
+  node.innerHTML = `<div class="ql-container ql-snow ql-rich-editor ql-read-only"><div class="ql-editor">${
     (quill as any).root.innerHTML
   }</div></div>`;
+
+  Array.from(node.querySelectorAll<HTMLElement>(".ql-code-block")).forEach(
+    (codeBlock) => {
+      codeBlock.innerHTML = (window as any).hljs.highlight(
+        codeBlock.getAttribute("data-language"),
+        codeBlock.innerText
+      ).value;
+    }
+  );
 }
 
 export function renderOps(node: HTMLElement, ops: Op[]) {
